9ddd00e9e946b8e2e7aae5f810b02fe23f8c7b7d
Halvor Smedås <stektpotet@gmail.com>
2022-08-23 21:13:07 +0200

    Add Admittance basic functionality

--- datatypes.py
+++ datatypes.py
@@ -2,9 +2,14 @@ from __future__ import annotations
 from dataclasses import dataclass, field
 import datetime
 from difflib import SequenceMatcher
+from itertools import permutations
+from typing import List, Tuple, Set
+from enum import Enum, Flag, IntFlag
+
 
 def _seq_ignore_space(c: str):
     return c in " \t\r\n"
+
 
 @dataclass(frozen=True, eq=True)
 class Person:
@@ -12,12 +17,17 @@ class Person:
     email: str
 
     def similar(self, other: Person, similarity_threshold: float = 0.9) -> bool:
-        # TODO: Consider reordering of names, last name before first name etc.
-        name_similarity = SequenceMatcher(_seq_ignore_space, self.name.strip().lower(),
-                                          other.name.strip().lower()).ratio()
-        email_similarity = SequenceMatcher(_seq_ignore_space, self.email.strip().lower(),
-                               other.email.strip().lower()).ratio()
+        # Consider permutations of name order, last name before first name etc.
+        name_similarity = 0
+        for name in (' '.join(names) for names in permutations(other.name.split(' '))):
+            if (ratio := SequenceMatcher(_seq_ignore_space, self.name, name).ratio()) > name_similarity:
+                name_similarity = ratio
+                if name_similarity > similarity_threshold:
+                    return True
+
+        email_similarity = SequenceMatcher(_seq_ignore_space, self.email, other.email).ratio()
         return name_similarity > similarity_threshold or email_similarity > similarity_threshold
+
 
 @dataclass(frozen=True)
 class Registration(Person):
@@ -25,11 +35,57 @@ class Registration(Person):
     timeslots: [str]
     read_rules: bool
 
+
 @dataclass(frozen=False)
 class RegistrationExtras:
-    year_of_studies: str
+    student_type: str
+    erasmus: bool
     nationality: str
-    sit_residence: bool
-    erasmus: bool = field(compare=False)
-    location: str
+    sit_residency: str
 
+
+class AdmittanceStatus(IntFlag):
+    Waiting_List = 0,
+    Admitted = 1,
+    Marked = 2
+
+
+
+class Timeslot:
+    capacity: int
+    spots: List[Person]
+    waiting_list: List[Person]
+    marked: Set[Person]
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.spots = []
+        self.waiting_list = []
+        self.marked = set()
+
+    def __str__(self):
+        content = ', '.join(f"{k}: {str(v)}" for k, v in self.__dict__.items())
+        return f"Timeslot({content})"
+
+    def __repr__(self):
+        content = ', '.join(f"{k}: {str(v)}" for k, v in self.__dict__.items())
+        return f"Timeslot({content})"
+
+    def admit(self, person: Person) -> Tuple[AdmittanceStatus, int]:
+        if (index := len(self.spots)) < self.capacity:
+            marked = False
+            for already_admitted in self.spots:
+                if person.similar(already_admitted):
+                    self.marked.add(person)
+                    # self.marked.add(already_admitted)
+                    marked = True
+                    break
+                    # TODO: Handle marked individuals
+
+            self.spots.append(person)
+            if marked:
+                return AdmittanceStatus.Admitted | AdmittanceStatus.Marked, index+1
+            return AdmittanceStatus.Admitted, index+1
+
+        self.waiting_list.append(person)
+        return AdmittanceStatus.Waiting_List, len(self.waiting_list)
--- main.py
+++ main.py
@@ -1,7 +1,82 @@
 import csv
 import itertools
-from datatypes import Person, Registration, RegistrationExtras
+from os import times
+from typing import List, Tuple
+
+from datatypes import Person, Registration, RegistrationExtras, Timeslot, AdmittanceStatus
 from datetime import datetime
+import tkinter as tk
+from tkinter import filedialog
+
+
+def _normalise(field: str) -> str:
+    return field.lower().strip()
+
+
+def read_entry(
+        timestamp: str,
+        email: str,
+        name: str,
+        timeslots: str,
+        student_type: str,
+        erasmus: str,
+        nationality: str,
+        sit_residency: str,
+        read_rules: str
+) -> Tuple[Registration, RegistrationExtras]:
+    registration = Registration(
+        _normalise(name),
+        _normalise(email),
+        datetime.strptime(timestamp, '%d/%m/%Y %H:%M:%S'),
+        [timeslot.replace(' ', '') for timeslot in timeslots.split(',')],
+        _normalise(read_rules) == "yes"
+    )
+    extras = RegistrationExtras(
+        _normalise(student_type),
+        _normalise(erasmus) == "yes",
+        _normalise(nationality),
+        _normalise(sit_residency)
+    )
+
+    return registration, extras
+
+
+def read_registrations(file_path: str) -> Tuple[List[Registration], List[RegistrationExtras]]:
+    with open(file_path, encoding="utf-8") as registration_file:
+        next(reader := csv.reader(registration_file))  # assign reader and skip headers
+        return tuple(zip(*[read_entry(*row) for row in reader]))
+
+timeslots = {
+    "10:00-11:00": Timeslot(50),
+    "11:00-12:00": Timeslot(60),
+    "12:00-13:00": Timeslot(70),
+    "13:00-14:00": Timeslot(80),
+}
 
 if __name__ == '__main__':
-    pass
+    root = tk.Tk()
+    root.withdraw()
+
+    file_path = filedialog.askopenfilename(filetypes=[("Comma Separated Values", "*.csv"), ("All types", "*.*")])
+    if file_path == '':
+        exit()
+
+    registrations, _ = read_registrations(file_path)
+    registrations = list(registrations)
+    registrations.sort(key=lambda reg: reg.timestamp)
+
+    for registration in registrations:
+        person = Person(registration.name, registration.email)
+        for wanted_slot in registration.timeslots:
+            status, spot = timeslots[wanted_slot].admit(person)
+            if AdmittanceStatus.Admitted in status:
+                if AdmittanceStatus.Marked in status:
+                    print("Marked!", registration, f"admitted to spot #{spot}")
+                break
+
+    print(timeslots)
+
+
+
+
+
